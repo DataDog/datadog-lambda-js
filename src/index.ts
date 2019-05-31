6e843b0ec2388b5877cfdf206230e86e5991e105
darcy.rayner <darcy.rayner@datadoghq.com>
2019-05-31 16:04:28 -0400

    Add configuration options for datadog lambda wrapper

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,45 @@ import { Handler } from "aws-lambda";
 import { patchHttp, readTraceContext, TraceContextService } from "./trace";
 
 /**
+ * Configuration options for DataDogs lambda wrapper.
+ */
+export interface Config {
+  // Whether to automatically patch all outgoing http requests with DataDog's
+  // hybrid tracing headers
+  autoPatchHTTP: boolean;
+}
+
+const defaultConfig: Config = {
+  autoPatchHTTP: true,
+} as const;
+
+/**
  * Wraps your AWS lambda handle functions to add tracing/metrics support
  * @param handler A lambda handler function
  */
-export function datadog<TEvent, TResult>(handler: Handler<TEvent, TResult>): Handler<TEvent, TResult> {
+export function datadog<TEvent, TResult>(
+  handler: Handler<TEvent, TResult>,
+  config?: Partial<Config>,
+): Handler<TEvent, TResult> {
+  const finalConfig = getConfig(config);
   const contextService = new TraceContextService();
-  patchHttp(contextService);
+
+  if (finalConfig.autoPatchHTTP) {
+    patchHttp(contextService);
+  }
 
   return (event, context, callback) => {
     contextService.rootTraceContext = readTraceContext(event, process.env);
     return handler(event, context, callback);
+  };
+}
+
+function getConfig(userConfig?: Partial<Config>): Config {
+  if (userConfig === undefined) {
+    return defaultConfig;
+  }
+  return {
+    ...defaultConfig,
+    ...userConfig,
   };
 }
