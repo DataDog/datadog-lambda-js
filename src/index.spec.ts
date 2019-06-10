473c4069146dc6890b694becef2ff981ce93010d
darcy.rayner <darcy.rayner@datadoghq.com>
2019-06-10 15:51:23 -0400

    Add ability for clients to send distribution metrics

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,37 +1,42 @@
 import http from "http";
 import nock from "nock";
 
-import { datadog } from "./index";
+import { datadog, sendDistributionMetric } from "./index";
+import { setErrorLoggingEnabled } from "./utils";
 
-<<<<<<< HEAD
-=======
-// tslint:disable-next-line: no-var-requires
-const nock = require("nock");
-
->>>>>>> master
 describe("datadog", () => {
   let traceId: string | undefined;
   let parentId: string | undefined;
   let sampled: string | undefined;
+  let oldEnv: typeof process.env;
 
-  const handler = (ev: any) => {
+  const handler = (ev: any, context: any, callback: any) => {
     // Mocks out the call
-
     const req = http.get("http://www.example.com");
     traceId = req.getHeader("x-datadog-trace-id") as string;
     parentId = req.getHeader("x-datadog-parent-id") as string;
     sampled = req.getHeader("x-datadog-sampling-priority") as string;
+    callback(null, "Result");
   };
   beforeEach(() => {
     traceId = undefined;
     parentId = undefined;
     sampled = undefined;
+    setErrorLoggingEnabled(false);
+    oldEnv = process.env;
+    process.env = { ...oldEnv };
+    nock.cleanAll();
   });
+  afterEach(() => {
+    process.env = oldEnv;
+  });
+
   it("patches http request when autoPatch enabled", () => {
     nock("http://www.example.com")
       .get("/")
       .reply(200, {});
-    datadog(handler)(
+    const wrapped = datadog(handler);
+    wrapped(
       {
         headers: {
           "x-datadog-parent-id": "9101112",
@@ -51,7 +56,8 @@ describe("datadog", () => {
     nock("http://www.example.com")
       .get("/")
       .reply(200, {});
-    datadog(handler, { autoPatchHTTP: false })(
+    const wrapped = datadog(handler, { autoPatchHTTP: false });
+    wrapped(
       {
         headers: {
           "x-datadog-parent-id": "9101112",
@@ -66,5 +72,67 @@ describe("datadog", () => {
     expect(traceId).toBeUndefined();
     expect(parentId).toBeUndefined();
     expect(sampled).toBeUndefined();
+  });
+
+  it("reads API keys from the environment for metrics", async () => {
+    const apiKey = "123456";
+    const apiKeyVar = "DD_API_KEY";
+    process.env[apiKeyVar] = apiKey;
+
+    nock("https://api.datadoghq.com")
+      .post(`/v1/series?api_key=${apiKey}`, (request: any) => request.series[0].metric === "my-dist")
+      .reply(200, {});
+
+    const wrapped = datadog(async () => {
+      sendDistributionMetric("my-dist", 100, "first-tag", "second-tag");
+      return "";
+    });
+    await wrapped({}, {} as any, () => {});
+
+    expect(nock.isDone()).toBeTruthy();
+  });
+
+  it("prefers API keys from the config object over the environment variable ", async () => {
+    const envApiKey = "123456";
+    const apiKeyVar = "DD_API_KEY";
+    process.env[apiKeyVar] = envApiKey;
+    const apiKey = "101112";
+
+    nock("https://api.datadoghq.com")
+      .post(`/v1/series?api_key=${apiKey}`, (request: any) => request.series[0].metric === "my-dist")
+      .reply(200, {});
+
+    const wrapped = datadog(
+      async () => {
+        sendDistributionMetric("my-dist", 100, "first-tag", "second-tag");
+        return "";
+      },
+      { apiKey },
+    );
+    await wrapped({}, {} as any, () => {});
+
+    expect(nock.isDone()).toBeTruthy();
+  });
+
+  it("reads site keys from the environment", async () => {
+    const site = "datadoghq.com";
+    const siteEnvVar = "DD_SITE";
+    const apiKey = "12345";
+    process.env[siteEnvVar] = site;
+
+    nock("https://api.datadoghq.com")
+      .post(`/v1/series?api_key=${apiKey}`, (request: any) => request.series[0].metric === "my-dist")
+      .reply(200, {});
+
+    const wrapped = datadog(
+      async () => {
+        sendDistributionMetric("my-dist", 100, "first-tag", "second-tag");
+        return "";
+      },
+      { apiKey },
+    );
+    await wrapped({}, {} as any, () => {});
+
+    expect(nock.isDone()).toBeTruthy();
   });
 });
