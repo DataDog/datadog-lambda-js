53d2f71e38ccdf0023969d7f2ccb7e4d9558f62f
darcy.rayner <darcy.rayner@datadoghq.com>
2019-06-28 11:02:37 -0400

    Update publish script

--- scripts/publish_prod.sh
+++ scripts/publish_prod.sh
@@ -7,7 +7,22 @@ if [ $BRANCH != "master" ]; then
     exit 1
 fi
 
-npm whoami
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo 'AWS_ACCESS_KEY_ID not set. Are you using aws-vault?'
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo 'AWS_SECRET_ACCESS_KEY not set. Are you using aws-vault?'
+    exit 1
+fi
+
+if [ -z "$AWS_SESSION_TOKEN" ]; then
+    echo 'AWS_SESSION_TOKEN not set. Are you using aws-vault?'
+    exit 1
+fi
+
+yarn login
 
 ./scripts/run_tests.sh
 
@@ -15,7 +30,7 @@ PACKAGE_VERSION=$(node -pe "require('./package.json').version")
 
 echo 'Publishing to Node'
 yarn build
-yarn publish --version "$PACKAGE_VERSION"
+yarn publish --new-version "$PACKAGE_VERSION"
 
 echo 'Tagging Release'
 git tag "v$PACKAGE_VERSION"
