5cc8c2b9bae30612e6880fa44714643e716e406f
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2019-08-14 15:14:30 -0400

    Use newer experimental API config option

--- src/trace/datadog-tracer.ts
+++ src/trace/datadog-tracer.ts
@@ -1,9 +1,10 @@
 import Tracer from "dd-trace";
+import { LOG_EXPORTER } from "dd-trace";
 
 export function initDatadogTracer() {
   Tracer.init({
     experimental: {
-      useLogWriter: true,
+      exporter: LOG_EXPORTER,
     },
   });
 }
