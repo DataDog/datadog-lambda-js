ab197c46890167defaede64929844163e6067e7b
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2019-09-16 10:48:34 -0400

    Add new log lines to datadog tracer

--- src/trace/datadog-tracer.ts
+++ src/trace/datadog-tracer.ts
@@ -1,9 +1,13 @@
 import Tracer from "dd-trace";
+import { logDebug } from "utils";
 
 const AWS_LAMBDA_FUNCTION_NAME = "AWS_LAMBDA_FUNCTION_NAME";
 
 export function initDatadogTracer() {
   const functionName = process.env[AWS_LAMBDA_FUNCTION_NAME];
+  if (functionName === undefined) {
+    logDebug(`Enviroment variable ${AWS_LAMBDA_FUNCTION_NAME} unavailable, using service name 'lambda'`);
+  }
   const service = functionName ? `${functionName}` : "lambda";
   Tracer.init({
     experimental: {
@@ -11,4 +15,5 @@ export function initDatadogTracer() {
     },
     service,
   });
+  logDebug(`Initialized datadog tracer for lambda`);
 }
