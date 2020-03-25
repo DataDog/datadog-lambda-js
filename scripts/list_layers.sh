9ee99deb3cbd2b23dc88b91767686fecf2d649cb
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-03-25 14:28:21 -0400

    Add ap-east-1 region to build scripts

--- scripts/list_layers.sh
+++ scripts/list_layers.sh
@@ -11,7 +11,7 @@
 set -e
 
 LAYER_NAMES=("Datadog-Node10-x" "Datadog-Node12-x")
-AVAILABLE_REGIONS=(us-east-2 us-east-1 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-north-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1)
+AVAILABLE_REGIONS=(us-east-2 us-east-1 us-west-1 us-west-2 ap-east-1 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-north-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1)
 
 # Check region arg
 if [ -z "$2" ]; then
