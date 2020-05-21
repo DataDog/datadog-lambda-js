4dabb1946c28472532788e96a632fcba4010046f
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-05-21 11:07:06 -0400

    Fix lint issue

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import { datadog, datadogHandlerEnvVar, lambdaTaskRootEnvVar, getEnvValue } from "./index";
 // We reuse the function loading logic already inside the lambda runtime.
+// tslint:disable-next-line:no-var-requires
 const { load } = require("/var/runtime/UserFunction") as any;
 
 const taskRootEnv = getEnvValue(lambdaTaskRootEnvVar, "");
