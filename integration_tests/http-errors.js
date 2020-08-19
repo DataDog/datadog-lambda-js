061eebbfaff734984cbab6c13a119ed5347d096c
Sergio Prada <chechopradaster@gmail.com>
2020-08-19 14:28:20 -0400

    add new plugin version and update snapshots

--- integration_tests/http-errors.js
+++ integration_tests/http-errors.js
@@ -1,19 +1,14 @@
 const { datadog, sendDistributionMetric } = require("datadog-lambda-js");
 const https = require("https");
 
-const paths = ["/400", "/500"];
-const urls = ["httpstat.us", "httpstat.us"];
-
 async function handle(event, context) {
   const responsePayload = { message: "hello, dog!" };
 
   sendDistributionMetric("serverless.integration_test.execution", 1, "function:http-request");
 
-  for (let index = 0; index < urls.length; index++) {
-    await httpsGet(urls[index], paths[index]);
-  }
+  await httpsGet('httpstat.us', '/400');
 
-  console.log(`Snapshot test http requests successfully made to URLs: ${urls}`);
+  console.log('Snapshot test http requests successfully made to URLs: httpstat.us/400');
 
   return responsePayload;
 }
