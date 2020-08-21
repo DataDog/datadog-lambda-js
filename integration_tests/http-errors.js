b2de6899d5b7d01e592c4a773013f874ca07c7ca
Sergio Prada <chechopradaster@gmail.com>
2020-08-21 07:55:50 -0400

    Add more integration tests (#94)
    * add plugin permutation
    
    * remove latest layers
    
    * add sorting for log diffing to improve flakyness
    
    * handle previous changes from merge
    
    * cleanup the script
    
    * remove stray console
    
    * use enableDDTracing:false to improve results
    
    * remove echo
    
    * cr feedback: remove noise of returned values
    
    * update plugin package
    
    * add new plugin version and update snapshots
    
    * fix flakiness using axios
    
    * fix hostname log
    
    * last snapshot update after fixing host v.s. hostname
    
    * clean up script
    
    * add yarn to install modules

--- integration_tests/http-errors.js
+++ integration_tests/http-errors.js
@@ -1,18 +1,18 @@
 const { datadog, sendDistributionMetric } = require("datadog-lambda-js");
 const axios = require("axios");
 
-const urls = ["https://ip-ranges.datadoghq.com", "https://ip-ranges.datadoghq.eu"];
-
 async function handle(event, context) {
   const responsePayload = { message: "hello, dog!" };
 
   sendDistributionMetric("serverless.integration_test.execution", 1, "function:http-request");
 
-  for (let index = 0; index < urls.length; index++) {
-    await axios({ url: urls[index], method: 'get' });
+  try {
+    await axios({ url: 'https://httpstat.us/400', method: 'get' });
+  } catch (err) {
+    return responsePayload;
   }
 
-  console.log(`Snapshot test http requests successfully made to URLs: ${urls}`);
+  console.log('Snapshot test http requests successfully made to URLs: https://httpstat.us/400');
 
   return responsePayload;
 }
