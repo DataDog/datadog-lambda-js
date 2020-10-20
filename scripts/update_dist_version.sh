19acc567ff3b84806746c4b36c8ce2187e207cda
Meghna Banerjee <meghna.banerjee@datadoghq.com>
2020-10-20 08:44:11 -0700

    add in yarn.lock logic

--- scripts/update_dist_version.sh
+++ scripts/update_dist_version.sh
@@ -2,6 +2,6 @@
 set -e
 echo "Updating version constants"
 DATADOG_LAMBDA_VERSION=$(cat package.json | jq -r ".version")
-DD_TRACE_VERSION=$(cat package.json | jq -r ."devDependencies"'."dd-trace"')
+DD_TRACE_VERSION=$(sed -n -E "s/dd-trace@([0-9]*.[0-9]*.[0-9]*):/\1/p" yarn.lock)
 sed -i -E "s/(datadogLambdaVersion = )\"(X.X.X)\"/\1\""$DATADOG_LAMBDA_VERSION"\"/" ./dist/trace/constants.js
 sed -i -E "s/(ddtraceVersion = )\"(X.X.X)\"/\1\""$DD_TRACE_VERSION"\"/" ./dist/trace/constants.js
