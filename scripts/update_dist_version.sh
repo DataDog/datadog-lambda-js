4e4f12c0145dbd10d95d4f46e09ff6ed312ace95
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-10-21 16:40:19 -0400

    Remove -i from sed command due to compatibility bugs

--- scripts/update_dist_version.sh
+++ scripts/update_dist_version.sh
@@ -3,5 +3,9 @@ set -e
 echo "Updating version constants"
 DATADOG_LAMBDA_VERSION=$(cat package.json | jq -r ".version")
 DD_TRACE_VERSION=$(sed -n -E "s/dd-trace@([0-9]*.[0-9]*.[0-9]*):/\1/p" yarn.lock)
-sed -i -E "s/(datadogLambdaVersion = )\"(X.X.X)\"/\1\""$DATADOG_LAMBDA_VERSION"\"/" ./dist/trace/constants.js
-sed -i -E "s/(ddtraceVersion = )\"(X.X.X)\"/\1\""$DD_TRACE_VERSION"\"/" ./dist/trace/constants.js
+echo "Lambda Version ${DATADOG_LAMBDA_VERSION}"
+echo "Trace Version ${DD_TRACE_VERSION}"
+
+cat ./dist/trace/constants.js |
+    sed -E "s/(datadogLambdaVersion = )\"(X.X.X)\"/\1\""$DATADOG_LAMBDA_VERSION"\"/" |
+    sed -E "s/(ddtraceVersion = )\"(X.X.X)\"/\1\""$DD_TRACE_VERSION"\"/" >> ./dist/trace/constants.js
