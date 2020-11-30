e0116d316018867dfdc4947236e4639f75423361
Darcy Rayner <50632605+DarcyRaynerDD@users.noreply.github.com>
2020-11-30 10:17:20 -0500

    Change extension detection mechanism (#131)
    * Change method of extension detection to use the filesystem
    
    * Fix agent path
    
    * Remove unused import
    
    * Change lambda tracing to not include extension flushing
    
    * Wrap handler correctly
    
    * Fix lint error

--- src/metrics/extension.ts
+++ src/metrics/extension.ts
@@ -1,11 +1,19 @@
 import { URL } from "url";
 import { get, post, logDebug } from "../utils";
+import fs from "fs";
 
 export const AGENT_URL = "http://127.0.0.1:8124";
 const HELLO_PATH = "/lambda/hello";
 const FLUSH_PATH = "/lambda/flush";
+const EXTENSION_PATH = "/opt/extensions/datadog-agent";
 const AGENT_TIMEOUT_MS = 100;
 export async function isAgentRunning() {
+  const extensionExists = await fileExists(EXTENSION_PATH);
+  if (!extensionExists) {
+    logDebug(`Agent isn't present in sandbox`);
+    return false;
+  }
+
   const url = new URL(HELLO_PATH, AGENT_URL);
   try {
     await get(url, { timeout: AGENT_TIMEOUT_MS });
@@ -25,4 +33,11 @@ export async function flushExtension() {
     return false;
   }
   return true;
+}
+
+function fileExists(filename: string): Promise<boolean> {
+  return fs.promises
+    .access(filename, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
 }
