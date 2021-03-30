6c5eeab0c317d9e945a261652156b2a724b0fe1f
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2021-03-30 15:14:03 -0400

    Raise uncompressed size limit

--- scripts/check_layer_size.sh
+++ scripts/check_layer_size.sh
@@ -9,7 +9,7 @@
 
 # 5 mb size limit
 MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 5 \* 1024) 
-MAX_LAYER_UNCOMPRESSED_SIZE=$(expr 13 \* 1024) 
+MAX_LAYER_UNCOMPRESSED_SIZE=$(expr 15 \* 1024) 
 
 
 LAYER_FILES_PREFIX="datadog_lambda_node"
