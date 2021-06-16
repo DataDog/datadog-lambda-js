7dc2f4363fd10fce02dc2f83e1148db8d9c01994
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-06-16 09:20:25 -0400

    Disable extension tracing

--- scripts/publish_sandbox.sh
+++ scripts/publish_sandbox.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+# Usage: VERSION=5 aws-vault exec sandbox-acccount-admin -- publish_layers.sh
 set -e
 
 ./scripts/build_layers.sh
 ./scripts/sign_layers.sh sandbox
-./scripts/publish_layers.sh sa-east-1
+VERSION=$VERSION REGIONS=sa-east-1 ./scripts/publish_layers.sh
