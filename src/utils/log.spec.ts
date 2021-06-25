b1747677165f00d9ee6f9cfce66845fa06cb79bf
chris.agocs <chris.agocs@datadoghq.com>
2021-06-25 15:39:41 -0400

    Add more logging tests (beleived to fail)

--- src/utils/log.spec.ts
+++ src/utils/log.spec.ts
@@ -12,4 +12,26 @@ describe("logger", () => {
 
     expect(logger.error).toHaveBeenLastCalledWith('{"status":"error","message":"datadog:My Error"}');
   });
+  it("logs errors correctly", async () => {
+    const logger = {
+      debug: jest.fn(),
+      error: jest.fn(),
+    };
+
+    setLogger(logger);
+    logError("My Error", new Error("Oh no!"));
+
+    expect(logger.error).toHaveBeenLastCalledWith('{"status":"error","message":"datadog:My Error"}');
+  });
+  it("logs errors and metadata correctly", async () => {
+    const logger = {
+      debug: jest.fn(),
+      error: jest.fn(),
+    };
+
+    setLogger(logger);
+    logError("My Error", { foo: "bar", baz: "2" }, new Error("Oh no 2!"));
+
+    expect(logger.error).toHaveBeenLastCalledWith('{"status":"error","message":"datadog:My Error"}');
+  });
 });
