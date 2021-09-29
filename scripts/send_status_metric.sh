9ce9b9eac30136880868546ae1f4f8381b202f19
maxday <maxime.david@datadoghq.com>
2021-09-29 12:38:18 -0400

    Send metric with integration tests status code

---
+++ scripts/send_status_metric.sh
@@ -0,0 +1,24 @@
+#Retrieve the API KEY and status
+
+DD_API_KEY=$1
+
+# 0 means success
+# 1 means failure
+STATUS=$2
+
+CURRENT_TIME=$(date +%s)
+
+#Send the metric
+curl  -X POST \
+-H "Content-type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-d "{ \"series\" :
+         [{\"metric\":\"serverless.integration_test.nodejs.status\",
+          \"points\":[[$CURRENT_TIME, $STATUS]],
+          \"tags\":[\"service:serverless-integration-test\"]}
+        ]
+    }" \
+'https://app.datadoghq.com/api/v1/series'
+
+#Propagate the status code so github action can know whether it's a success or failure
+exit $STATUS
