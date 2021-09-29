73409c4ea8084d61974cc5b057ef500612be15f6
maxday <maxime.david@datadoghq.com>
2021-09-29 15:12:53 -0400

    api key

--- scripts/send_status_metric.sh
+++ scripts/send_status_metric.sh
@@ -2,11 +2,13 @@
 # 0 means success
 # 1 means failure
 STATUS=$1
+
+API_KEY=$2
 CURRENT_TIME=$(date +%s)
 
 #Send the metric
 curl -H "Content-type: application/json" \
--H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-API-KEY: ${API_KEY}" \
 -d "{ \"series\" :
          [{\"metric\":\"serverless.integration_test.nodejs.status\",
           \"points\":[[$CURRENT_TIME, $STATUS]],
