d969973379bd59d5687d48be3ae81d7fc267cbb0
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-10-20 14:23:44 -0400

    Tweaks

--- src/runtime/user-function.ts
+++ src/runtime/user-function.ts
@@ -1,6 +1,6 @@
 /**
  * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Modifictions copyright 2021 Datadog, Inc.
+ * Modifications copyright 2021 Datadog, Inc.
  * 
  * The original file was part of aws-lambda-nodejs-runtime-interface-client
  * https://github.com/aws/aws-lambda-nodejs-runtime-interface-client/blob/main/src/utils/UserFunction.ts
