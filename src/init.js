3f3f82a3a3dbcd9da2874810676e0c8ed2bc827a
Darcy Rayner <darcy.rayner@datadoghq.com>
2022-10-04 13:50:19 -0400

    Update init to work with relative directories

--- src/init.js
+++ src/init.js
@@ -1,3 +1,3 @@
 
 process.env['DD_LAMBDA_HANDLER'] = process.env["_HANDLER"];
-process.env["_HANDLER"] = '/opt/nodejs/node_modules/datadog-lambda-js/handler.handler';
+process.env["_HANDLER"] = `${__dirname}/handler.handler`;
