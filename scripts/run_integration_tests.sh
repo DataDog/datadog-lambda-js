38b748c4ec7f9576a83da2e6b529ec9acc24dd70
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2022-10-19 14:10:02 -0400

    fix run_integration_tests.sh

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -61,9 +61,9 @@ fi
 if [ -n "$BUILD_LAYERS" ]; then
     echo "Building layers that will be deployed with our test functions"
     if [ -n "$BUILD_LAYER_VERSION" ]; then
-        source $scripts_dir/build_layers.sh
-    else
         NODE_VERSION=${!BUILD_LAYER_VERSION} source $scripts_dir/build_layers.sh
+    else
+        source $scripts_dir/build_layers.sh
     fi
 else
     echo "Not building layers, ensure they've already been built or re-run with 'BUILD_LAYERS=true DD_API_KEY=XXXX ./scripts/run_integration_tests.sh'"
