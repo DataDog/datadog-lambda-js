81696d48181034c54ac9db3b844321ba98419e87
AJ Stuyvenberg <astuyve@gmail.com>
2023-01-26 10:51:05 -0500

    17mb unzipped

--- scripts/check_layer_size.sh
+++ scripts/check_layer_size.sh
@@ -9,7 +9,7 @@
 
 # 6 mb size limit
 MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 6 \* 1024)
-MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 16 \* 1024)
+MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 17 \* 1024)
 
 
 LAYER_FILES_PREFIX="datadog_lambda_node"
