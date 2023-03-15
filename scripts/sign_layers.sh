76021dcba883e7cb9e6c960659ba5742e20ebac7
AJ Stuyvenberg <astuyve@gmail.com>
2023-03-15 16:13:33 -0400

    feat: Use serverless sandbox bucket (#366)

--- scripts/sign_layers.sh
+++ scripts/sign_layers.sh
@@ -23,7 +23,7 @@ if [[ ! "${VALID_ACCOUNTS[@]}" =~ $1 ]]; then
 fi
 if [ "$1" = "sandbox" ]; then
     REGION="sa-east-1"
-    S3_BUCKET_NAME="dd-lambda-signing-bucket-sandbox"
+    S3_BUCKET_NAME="dd-lambda-signing-bucket-serverless-sandbox"
 fi
 if [ "$1" = "prod" ]; then
     REGION="us-east-1"
