dbb48e72225164e6263a32c9cabd1da89556dfc6
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-04-07 17:26:01 -0400

    add response streaming constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
 export const datadogLambdaVersion = "X.X.X";
+
+// Response streaming functions
+export const HANDLER_STREAMING = Symbol.for("aws.lambda.runtime.handler.streaming");
+export const STREAM_RESPONSE = "response";
