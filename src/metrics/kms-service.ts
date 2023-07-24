efc220d41ff830d489291e4170d1b30df38463d5
AJ Stuyvenberg <astuyve@gmail.com>
2023-07-24 13:27:35 -0400

    feat: lint ignore

--- src/metrics/kms-service.ts
+++ src/metrics/kms-service.ts
@@ -44,8 +44,11 @@ export class KMSService {
 
   // Node 18 or AWS SDK V3
   public async decryptV3(buffer: Buffer): Promise<string> {
+    // tslint:disable-next-line: variable-name
+    // tslint:disable one-variable-per-declaration
     let KMSClient, DecryptCommand;
     try {
+
       ({ KMSClient, DecryptCommand } = require("@aws-sdk/client-kms"));
     } catch (e) {
       throw Error("Can't load AWS SDK v2 or v3 to decrypt KMS key, custom metrics may not be sent");
