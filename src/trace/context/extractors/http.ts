5b29f1876349ab88371fdeaada69810c1e7c03ec
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-11-06 20:11:32 -0500

    add `http.ts` extractor
    refactoring code to make sure we correctly handling an HTTP event for trace context

---
+++ src/trace/context/extractors/http.ts
@@ -0,0 +1,95 @@
+import { EventValidator } from "../../../utils/event-validator";
+import { logDebug } from "../../../utils";
+import { EventTraceExtractor } from "../extractor";
+import { TracerWrapper } from "../../tracer-wrapper";
+import { SpanContextWrapper } from "../../span-context-wrapper";
+
+const AUTHORIZING_REQUEST_ID_HEADER = "x-datadog-authorizing-requestid";
+
+export enum HTTPEventSubType {
+  ApiGatewayV1 = "api-gateway-rest-api",
+  ApiGatewayV2 = "api-gateway-http-api",
+  ApiGatewayWebSocket = "api-gateway-websocket",
+  Unknown = "unknown-sub-type",
+}
+
+export class HTTPEventTraceExtractor implements EventTraceExtractor {
+  constructor(private tracerWrapper: TracerWrapper, private decodeAuthorizerContext: boolean = true) {
+    this.decodeAuthorizerContext = decodeAuthorizerContext;
+  }
+
+  extract(event: any): SpanContextWrapper | null {
+    if (this.decodeAuthorizerContext) {
+      // need to set the trace context if using authorizer lambda in authorizing (non-cached) cases
+      try {
+        const eventSourceSubType: HTTPEventSubType = HTTPEventTraceExtractor.getEventSubType(event);
+        const injectedAuthorizerHeaders = HTTPEventTraceExtractor.getInjectedAuthorizerHeaders(
+          event,
+          eventSourceSubType,
+        );
+        if (injectedAuthorizerHeaders !== null) {
+          const _traceContext = this.tracerWrapper.extract(injectedAuthorizerHeaders);
+          if (_traceContext === null) return null;
+
+          logDebug(`Extracted trace context from authorizer event`, { traceContext: _traceContext, event });
+          return _traceContext;
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          logDebug(`Unable to extract trace context from authorizer event.`, error);
+        }
+      }
+    }
+
+    const headers = event.headers;
+    const lowerCaseHeaders: { [key: string]: string } = {};
+
+    for (const key of Object.keys(headers)) {
+      lowerCaseHeaders[key.toLowerCase()] = headers[key];
+    }
+
+    const traceContext = this.tracerWrapper.extract(lowerCaseHeaders);
+    if (traceContext === null) return null;
+
+    logDebug(`Extracted trace context from HTTP event`, { traceContext, event });
+    return traceContext;
+  }
+
+  public static getEventSubType(event: any): HTTPEventSubType {
+    if (EventValidator.isAPIGatewayEvent(event)) {
+      return HTTPEventSubType.ApiGatewayV1;
+    }
+
+    if (EventValidator.isAPIGatewayEventV2(event)) {
+      return HTTPEventSubType.ApiGatewayV2;
+    }
+
+    if (EventValidator.isAPIGatewayWebSocketEvent(event)) {
+      return HTTPEventSubType.ApiGatewayWebSocket;
+    }
+
+    return HTTPEventSubType.Unknown;
+  }
+
+  public static getInjectedAuthorizerHeaders(event: any, eventSubType: HTTPEventSubType) {
+    const authorizerHeaders = event?.requestContext?.authorizer;
+    if (!authorizerHeaders) return null;
+
+    let rawDatadogData = authorizerHeaders._datadog;
+    if (eventSubType === HTTPEventSubType.ApiGatewayV2) {
+      rawDatadogData = authorizerHeaders.lambda._datadog;
+    }
+    if (!rawDatadogData) return null;
+
+    const injectedData = JSON.parse(Buffer.from(rawDatadogData, "base64").toString());
+
+    if (
+      authorizerHeaders.integrationLatency > 0 ||
+      event.requestContext.requestId === injectedData[AUTHORIZING_REQUEST_ID_HEADER]
+    ) {
+      return injectedData;
+    }
+
+    return null;
+  }
+}
