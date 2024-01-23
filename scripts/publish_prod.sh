e69de718fe737c3bd719a317b668aa1d2268f152
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-01-23 11:53:57 -0500

    adjust script to work only for one environment

--- scripts/publish_prod.sh
+++ scripts/publish_prod.sh
@@ -31,16 +31,6 @@ else
     NEW_VERSION=$1
 fi
 
-echo "Ensure you have access to the datadog NPM service account"
-yarn login
-
-echo "Ensure you have access to the AWS GovCloud account"
-ddsaml2aws login -a govcloud-us1-fed-human-engineering
-AWS_PROFILE=govcloud-us1-fed-human-engineering aws sts get-caller-identity
-
-echo "Ensure you have access to the production AWS account"
-aws-vault exec sso-prod-engineering -- aws sts get-caller-identity
-
 CURRENT_VERSION=$(npm view datadog-lambda-js version)
 LAYER_VERSION=$(echo $NEW_VERSION | cut -d '.' -f 2)
 
@@ -50,8 +40,13 @@ if [ "$CONT" != "y" ]; then
     exit 1
 fi
 
-# Update the version without committing
-yarn version --no-git-tag-version --new-version $NEW_VERSION
+read -p "Update package.json version? (y/n)?" CONT
+if [ "$CONT" != "y" ]; then
+    echo "Skipping updating package.json version"
+else
+    # Update the version without committing
+    yarn version --no-git-tag-version --new-version $NEW_VERSION
+fi
 
 echo
 echo 'Building layers...'
@@ -61,38 +56,68 @@ echo
 echo "Signing layers for commercial AWS regions"
 aws-vault exec sso-prod-engineering -- ./scripts/sign_layers.sh prod
 
-echo
-echo "Publishing layers to commercial AWS regions"
-VERSION=$LAYER_VERSION aws-vault exec sso-prod-engineering --no-session -- ./scripts/publish_layers.sh
+read -p "Deploy layers to commercial AWS (y/n)?" CONT
+if [ "$CONT" != "y" ]; then
+    echo "Skipping deployment to commercial AWS"
+else
+    echo "Ensuring you have access to the production AWS account"
+    aws-vault exec sso-prod-engineering -- aws sts get-caller-identity
 
-echo "Publishing layers to GovCloud AWS regions"
-ddsaml2aws login -a govcloud-us1-fed-human-engineering
-VERSION=$LAYER_VERSION AWS_PROFILE=govcloud-us1-fed-human-engineering ./scripts/publish_layers.sh
+    echo
+    echo "Publishing layers to commercial AWS regions"
+    VERSION=$LAYER_VERSION aws-vault exec sso-prod-engineering --no-session -- ./scripts/publish_layers.sh
+fi
+
+read -p "Deploy layers to GovCloud AWS (y/n)?" CONT
+if [ "$CONT" != "y" ]; then
+    echo "Skipping deployment to GovCloud AWS"
+else
+    echo "Ensuring you have access to the AWS GovCloud account"
+    ddsaml2aws login -a govcloud-us1-fed-human-engineering
+    AWS_PROFILE=govcloud-us1-fed-human-engineering aws sts get-caller-identity
+
+    echo "Publishing layers to GovCloud AWS regions"
+    ddsaml2aws login -a govcloud-us1-fed-human-engineering
+    VERSION=$LAYER_VERSION AWS_PROFILE=govcloud-us1-fed-human-engineering ./scripts/publish_layers.sh
+fi
 
 read -p "Ready to publish $NEW_VERSION to NPM (y/n)?" CONT
 if [ "$CONT" != "y" ]; then
-    echo "Exiting"
-    exit 1
+    echo "Skipping publishing to NPM"
+else
+    echo "Ensuring you have access to the datadog NPM service account"
+    yarn login
+
+    echo 'Publishing to NPM'
+    if [ -d "./dist" ]; then
+        rm -rf ./dist
+    fi
+    yarn build
+    cp ./dist/handler.cjs ./dist/handler.js
+    yarn publish --new-version "$NEW_VERSION"
 fi
 
-echo 'Publishing to NPM'
-if [ -d "./dist" ]; then
-    rm -rf ./dist
+read -p "Do you want to bump the version in GitHub? (y/n)" CONT
+if [ "$CONT" != "y" ]; then
+    echo "Skipping publishing updates to GitHub"
+else
+    echo
+    echo 'Publishing updates to GitHub'
+    git commit package.json -m "Bump version to ${NEW_VERSION}"
+    git push origin main
+    git tag "v$NEW_VERSION"
+    git push origin "refs/tags/v$NEW_VERSION"
+
+    echo
+    echo "Now create a new release with the tag v${NEW_VERSION} created"
+    echo "https://github.com/DataDog/datadog-lambda-js/releases/new?tag=v$NEW_VERSION&title=v$NEW_VERSION"
 fi
-yarn build
-cp ./dist/handler.cjs ./dist/handler.js
-yarn publish --new-version "$NEW_VERSION"
 
-echo
-echo 'Publishing updates to github'
-git commit package.json -m "Bump version to ${NEW_VERSION}"
-git push origin main
-git tag "v$NEW_VERSION"
-git push origin "refs/tags/v$NEW_VERSION"
 
-echo
-echo "Now create a new release with the tag v${NEW_VERSION} created"
-echo "https://github.com/DataDog/datadog-lambda-js/releases/new?tag=v$NEW_VERSION&title=v$NEW_VERSION"
-
-# Open a PR to the documentation repo to automatically bump layer version
-VERSION=$LAYER_VERSION LAYER=datadog-lambda-js ./scripts/create_documentation_pr.sh
+read -p "Do you want to create a documentation PR for the new layer? (y/n)" CONT
+if [ "$CONT" != "y" ]; then
+    echo "Skipping publishing updates to GitHub"
+else
+    # Open a PR to the documentation repo to automatically bump layer version
+    VERSION=$LAYER_VERSION LAYER=datadog-lambda-js ./scripts/create_documentation_pr.sh
+fi
