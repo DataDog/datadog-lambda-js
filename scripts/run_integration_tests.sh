9d69392da27ca11f7afe7b07caf97a5530d341da
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-02-12 16:38:38 -0500

    remove it from scripts

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -29,12 +29,11 @@ mismatch_found=false
 # [0]: serverless runtime name
 # [1]: nodejs version
 # [2]: random 8-character ID to avoid collisions with other runs
-node14=("nodejs14.x" "14.15" $(xxd -l 4 -c 4 -p < /dev/random))
 node16=("nodejs16.x" "16.14" $(xxd -l 4 -c 4 -p < /dev/random))
 node18=("nodejs18.x" "18.12" $(xxd -l 4 -c 4 -p < /dev/random))
 node20=("nodejs20.x" "20.9" $(xxd -l 4 -c 4 -p < /dev/random))
 
-PARAMETERS_SETS=("node14" "node16" "node18" "node20")
+PARAMETERS_SETS=("node16" "node18" "node20")
 
 if [ -z "$RUNTIME_PARAM" ]; then
     echo "Node version not specified, running for all node versions."
