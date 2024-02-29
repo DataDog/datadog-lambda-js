f8912fc88a7011ecee4897774731dc50a9090b16
nagydaoudt <nagy.daoud@deliveryhero.com>
2024-02-29 14:58:54 +0100

    removing DD_COMPRESS_METRIC_PAYLOAD env var

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,6 @@ export const coldStartTracingEnvVar = "DD_COLD_START_TRACING";
 export const minColdStartTraceDurationEnvVar = "DD_MIN_COLD_START_DURATION";
 export const coldStartTraceSkipLibEnvVar = "DD_COLD_START_TRACE_SKIP_LIB";
 export const localTestingEnvVar = "DD_LOCAL_TESTING";
-export const compressPayloadEnvVar = "DD_COMPRESS_METRIC_PAYLOAD";
 
 interface GlobalConfig {
   /**
@@ -355,11 +354,6 @@ function getConfig(userConfig?: Partial<Config>): Config {
   if (userConfig === undefined || userConfig.decodeAuthorizerContext === undefined) {
     const result = getEnvValue(decodeAuthorizerContextEnvVar, "true").toLowerCase();
     config.decodeAuthorizerContext = result === "true";
-  }
-
-  if (userConfig === undefined || userConfig.compressPayload === undefined) {
-    const result = getEnvValue(compressPayloadEnvVar, "true").toLowerCase();
-    config.compressPayload = result === "true";
   }
 
   if (userConfig === undefined || userConfig.minColdStartTraceDuration === undefined) {
