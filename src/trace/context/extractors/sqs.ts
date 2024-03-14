5184c051d5caa532141cc3e1737da06912f0f16a
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2024-03-14 14:01:09 -0400

    extrace trace context from AWSTraceHeader in SQS java upstream case

--- src/trace/context/extractors/sqs.ts
+++ src/trace/context/extractors/sqs.ts
@@ -3,18 +3,29 @@ import { EventTraceExtractor } from "../extractor";
 import { TracerWrapper } from "../../tracer-wrapper";
 import { logDebug } from "../../../utils";
 import { SpanContextWrapper } from "../../span-context-wrapper";
+import { XrayService } from "../../xray-service";
 
 export class SQSEventTraceExtractor implements EventTraceExtractor {
   constructor(private tracerWrapper: TracerWrapper) {}
 
   extract(event: SQSEvent): SpanContextWrapper | null {
-    const headers = event?.Records?.[0]?.messageAttributes?._datadog?.stringValue;
-    if (headers === undefined) return null;
-
     try {
-      const traceContext = this.tracerWrapper.extract(JSON.parse(headers));
-      if (traceContext === null) return null;
+      var prepared_headers;
+      const headers = event?.Records?.[0]?.messageAttributes?._datadog?.stringValue;
+      if (headers !== undefined) {
+        prepared_headers = JSON.parse(headers);
+      } else {
+        if (event?.Records?.[0]?.attributes?.AWSTraceHeader) {
+          prepared_headers = XrayService.extraceDDContextFromAWSTraceHeader(event.Records[0].attributes.AWSTraceHeader);
+        }
+      }
 
+      if (!prepared_headers) return null;
+      const traceContext = this.tracerWrapper.extract(prepared_headers);
+      if (traceContext === null) {
+        logDebug("Unable to extract the injected trace context from event");
+        return null;
+      }
       logDebug(`Extracted trace context from SQS event`, { traceContext, event });
       return traceContext;
     } catch (error) {
