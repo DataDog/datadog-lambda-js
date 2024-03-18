d32051c811edb3edafe03aa42427af6977a781df
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-03-18 11:45:12 -0400

    move nested `if` in `else` to `else-if`

--- src/trace/context/extractors/sqs.ts
+++ src/trace/context/extractors/sqs.ts
@@ -14,19 +14,18 @@ export class SQSEventTraceExtractor implements EventTraceExtractor {
       const headers = event?.Records?.[0]?.messageAttributes?._datadog?.stringValue;
       if (headers !== undefined) {
         parsedHeaders = JSON.parse(headers);
-      } else {
-        if (event?.Records?.[0]?.attributes?.AWSTraceHeader) {
-          parsedHeaders = XrayService.extraceDDContextFromAWSTraceHeader(event.Records[0].attributes.AWSTraceHeader);
-        }
+      } else if (event?.Records?.[0]?.attributes?.AWSTraceHeader !== undefined) {
+        parsedHeaders = XrayService.extraceDDContextFromAWSTraceHeader(event.Records[0].attributes.AWSTraceHeader);
       }
-
       if (!parsedHeaders) return null;
+
       const traceContext = this.tracerWrapper.extract(parsedHeaders);
       if (traceContext === null) {
-        logDebug(`Failed to extract trace context from prepared headers: ${parsedHeaders}`);
+        logDebug("Failed to extract trace context from parsed headers", { parsedHeaders, event });
         return null;
       }
-      logDebug(`Extracted trace context from SQS event`, { traceContext, event });
+
+      logDebug("Extracted trace context from SQS event", { traceContext, event });
       return traceContext;
     } catch (error) {
       if (error instanceof Error) {
