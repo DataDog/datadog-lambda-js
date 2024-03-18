b16ef2fed3f58aaec5cf19b723d9306cac737b34
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-03-18 16:59:04 -0400

    remove local flush from extension code

--- src/metrics/extension.ts
+++ src/metrics/extension.ts
@@ -1,26 +1,13 @@
-import { URL } from "url";
-import { post, logDebug, logError } from "../utils";
+import { logDebug } from "../utils";
 import fs from "fs";
 
 export const AGENT_URL = "http://127.0.0.1:8124";
-const FLUSH_PATH = "/lambda/flush";
 const EXTENSION_PATH = "/opt/extensions/datadog-agent";
-const AGENT_TIMEOUT_MS = 100;
 
 export async function isAgentRunning() {
   const extensionExists = await fileExists(EXTENSION_PATH);
   if (!extensionExists) {
     logDebug(`Agent isn't present in sandbox`);
-    return false;
-  }
-  return true;
-}
-
-export async function flushExtension(): Promise<boolean> {
-  const url = new URL(FLUSH_PATH, AGENT_URL);
-  const result = await post(url, {}, { timeout: AGENT_TIMEOUT_MS });
-  if (!result.success) {
-    logError(`Failed to flush extension. ${result.errorMessage}`);
     return false;
   }
   return true;
