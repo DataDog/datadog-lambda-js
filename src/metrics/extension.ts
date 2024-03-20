dbc4ca416191ed354379f249629100bf706176b2
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-03-20 00:09:28 -0400

    rename `agent` to `extension`

--- src/metrics/extension.ts
+++ src/metrics/extension.ts
@@ -2,12 +2,12 @@ import { URL } from "url";
 import { post, logDebug, logError } from "../utils";
 import fs from "fs";
 
-export const AGENT_URL = "http://127.0.0.1:8124";
-const FLUSH_PATH = "/lambda/flush";
+export const EXTENSION_URL = "http://127.0.0.1:8124";
 const EXTENSION_PATH = "/opt/extensions/datadog-agent";
-const AGENT_TIMEOUT_MS = 100;
+const LOCAL_FLUSH_PATH = "/lambda/flush";
+const LOCAL_FLUSH_TIMEOUT_MS = 100;
 
-export async function isAgentRunning() {
+export async function isExtensionRunning() {
   const extensionExists = await fileExists(EXTENSION_PATH);
   if (!extensionExists) {
     logDebug(`Agent isn't present in sandbox`);
@@ -17,8 +17,8 @@ export async function isAgentRunning() {
 }
 
 export async function flushExtension(): Promise<boolean> {
-  const url = new URL(FLUSH_PATH, AGENT_URL);
-  const result = await post(url, {}, { timeout: AGENT_TIMEOUT_MS });
+  const url = new URL(LOCAL_FLUSH_PATH, EXTENSION_URL);
+  const result = await post(url, {}, { timeout: LOCAL_FLUSH_TIMEOUT_MS });
   if (!result.success) {
     logError(`Failed to flush extension. ${result.errorMessage}`);
     return false;
