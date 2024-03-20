e08ad86b20db3b18337695f06c547b6b1d793758
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-03-20 10:25:18 -0400

    chore: rename Agent to Extension (#520)
    * rename `agent` to `extension`
    
    * update imports
    
    * update tests
    
    * more renaming

--- src/metrics/extension.spec.ts
+++ src/metrics/extension.spec.ts
@@ -1,35 +1,35 @@
 import nock from "nock";
 
-import { isAgentRunning, flushExtension, AGENT_URL } from "./extension";
+import { isExtensionRunning, flushExtension, EXTENSION_URL } from "./extension";
 import mock from "mock-fs";
 
-describe("isAgentRunning", () => {
+describe("isExtensionRunning", () => {
   afterEach(() => {
     mock.restore();
   });
-  it("returns true when agent exists and responds", async () => {
+  it("returns true when extension exists and responds", async () => {
     mock({
       "/opt/extensions/datadog-agent": Buffer.from([0]),
     });
-    const ran = await isAgentRunning();
+    const ran = await isExtensionRunning();
     expect(ran).toBeTruthy();
   });
-  it("returns false when agent doesn't exist", async () => {
+  it("returns false when extension doesn't exist", async () => {
     mock({});
-    const scope = nock(AGENT_URL).get("/lambda/hello").replyWithError("Unreachable");
-    const ran = await isAgentRunning();
+    const scope = nock(EXTENSION_URL).get("/lambda/hello").replyWithError("Unreachable");
+    const ran = await isExtensionRunning();
     expect(scope.isDone()).toBeFalsy();
     expect(ran).toBeFalsy();
   });
 });
 describe("flushExtension", () => {
-  it("calls flush on the agent", async () => {
-    const scope = nock(AGENT_URL).post("/lambda/flush", JSON.stringify({})).reply(200);
+  it("calls flush on the extension", async () => {
+    const scope = nock(EXTENSION_URL).post("/lambda/flush", JSON.stringify({})).reply(200);
     await flushExtension();
     expect(scope.isDone()).toBeTruthy();
   });
   it("catches error when flush doesn't respond", async () => {
-    const scope = nock(AGENT_URL).post("/lambda/flush", JSON.stringify({})).replyWithError("Unavailable");
+    const scope = nock(EXTENSION_URL).post("/lambda/flush", JSON.stringify({})).replyWithError("Unavailable");
     await flushExtension();
     expect(scope.isDone()).toBeTruthy();
   });
