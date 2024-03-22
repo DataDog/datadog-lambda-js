1713b099057a4399503857bff71100ba46f93f56
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-03-22 16:39:55 -0400

    export `MetricsQueue` from `metrics`

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -1,3 +1,4 @@
 export { MetricsConfig, MetricsListener } from "./listener";
+export { MetricsQueue } from './queue'
 export { KMSService } from "./kms-service";
 export { incrementErrorsMetric, incrementInvocationsMetric } from "./enhanced-metrics";
