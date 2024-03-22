d2dbe4c78df92a017b4ebbf93d5039a6c4d0d5a0
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-03-22 16:37:50 -0400

    add `warning` level to logger

--- src/utils/log.spec.ts
+++ src/utils/log.spec.ts
@@ -1,10 +1,11 @@
-import { setLogger, logError } from "./log";
+import { setLogger, logError, logDebug, logWarning } from "./log";
 
 describe("logger", () => {
   it("log using custom logger", async () => {
     const logger = {
       debug: jest.fn(),
       error: jest.fn(),
+      warn: jest.fn()
     };
 
     setLogger(logger);
@@ -12,10 +13,12 @@ describe("logger", () => {
 
     expect(logger.error).toHaveBeenLastCalledWith('{"status":"error","message":"datadog:My Error"}');
   });
+
   it("logs errors correctly", async () => {
     const logger = {
       debug: jest.fn(),
       error: jest.fn(),
+      warn: jest.fn()
     };
 
     setLogger(logger);
@@ -23,10 +26,12 @@ describe("logger", () => {
     const lastErrorCall = logger.error.mock.calls[0][0];
     expect(lastErrorCall).toContain('{"status":"error","message":"Oh no!","name":"Error","stack":"Error: Oh no!');
   });
+
   it("logs errors and metadata correctly", async () => {
     const logger = {
       debug: jest.fn(),
       error: jest.fn(),
+      warn: jest.fn()
     };
 
     setLogger(logger);
@@ -36,5 +41,18 @@ describe("logger", () => {
     expect(lastErrorCall).toContain(
       '{"status":"error","message":"Oh no 2!","foo":"bar","baz":"2","name":"Error","stack":"Error: Oh no 2!',
     );
+  });
+
+  it("logs warnings correctly", async () => {
+    const logger = {
+      debug: jest.fn(),
+      error: jest.fn(),
+      warn: jest.fn()
+    };
+
+    setLogger(logger);
+    logWarning("This is a warning");
+    const lastWarning = logger.warn.mock.calls[0][0];
+    expect(lastWarning).toContain('{"status":"warning","message":"datadog:This is a warning"}');
   });
 });
