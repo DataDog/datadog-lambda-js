2dab46c6b1998617193bd48d7d22f00419856dc1
Scott Solmonson <scott.solmonson@nlb-int-svc-proxy-06a01ef5ff9ffd47.elb.us-gov-west-1.amazonaws.com>
2024-04-05 20:24:02 -0400

    format and lint files

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,9 +1,12 @@
 export interface LambdaResponse {
-    batchItemFailures?: Array<{ itemIdentifier: string }>
+  batchItemFailures?: { itemIdentifier: string }[];
 }
 
 export function isBatchItemFailure(lambdaResponse: any): lambdaResponse is LambdaResponse {
-    return (typeof lambdaResponse === 'object' && 'batchItemFailures' in lambdaResponse &&
-    Array.isArray(lambdaResponse.batchItemFailures) && lambdaResponse.batchItemFailures.some((item: { itemIdentifier: any }) => typeof item.itemIdentifier === 'string')
-    );
-} 
+  return (
+    typeof lambdaResponse === "object" &&
+    "batchItemFailures" in lambdaResponse &&
+    Array.isArray(lambdaResponse.batchItemFailures) &&
+    lambdaResponse.batchItemFailures.some((item: { itemIdentifier: any }) => typeof item.itemIdentifier === "string")
+  );
+}
