cb26f0b0c6b395a4f68c3c60fd9a5436d3ef1d7b
William Conti <william.conti@datadoghq.com>
2024-08-01 13:36:41 -0400

    fix lint

--- src/utils/dd_tags.ts
+++ src/utils/dd_tags.ts
@@ -1,5 +1,5 @@
 export function updateDDTags(newPair: string): void {
-    const tags = process.env.DD_TAGS ?? '';
-    const updatedEnv = tags ? `${tags.trim().replace(/,+$/, '')},${newPair}` : newPair;
-    process.env.DD_TAGS = updatedEnv;
+  const tags = process.env.DD_TAGS ?? "";
+  const updatedEnv = tags ? `${tags.trim().replace(/,+$/, "")},${newPair}` : newPair;
+  process.env.DD_TAGS = updatedEnv;
 }
