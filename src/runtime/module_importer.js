40ed022a3beedad5046a6f73d0924c838666debb
William Conti <william.conti@datadoghq.com>
2024-08-01 14:01:48 -0400

    update tags code object instead

--- src/runtime/module_importer.js
+++ src/runtime/module_importer.js
@@ -13,8 +13,8 @@ exports.initTracer = function () {
     const path = require.resolve("dd-trace", { paths: ["/var/task/node_modules", ...module.paths] });
     // tslint:disable-next-line:no-var-requires
     // add lambda tags to DD_TAGS environment variable
-    updateDDTags("_dd.origin:lambda")
-    const tracer = require(path).init();
+    const ddtags = updateDDTags({"_dd.origin": "lambda"})
+    const tracer = require(path).init({tags: ddtags});
     logDebug("automatically initialized dd-trace");
 
     // Configure the tracer to ignore HTTP calls made from the Lambda Library to the Extension
--- src/utils/dd_tags.ts
+++ src/utils/dd_tags.ts
@@ -1,5 +1,12 @@
-export function updateDDTags(newPair: string): void {
-  const tags = process.env.DD_TAGS ?? "";
-  const updatedEnv = tags ? `${tags.trim().replace(/,+$/, "")},${newPair}` : newPair;
-  process.env.DD_TAGS = updatedEnv;
+export function updateDDTags(newTags: Record<string, any> = {}): Record<string, any> {
+  const envTags = (process.env.DD_TAGS ?? "")
+    .split(",")
+    .filter((pair) => pair.includes(":"))
+    .reduce((acc: Record<string, any>, pair: string) => {
+      const [key, value] = pair.split(":");
+      if (key && value) acc[key] = value;
+      return acc;
+    }, {});
+
+  return { ...envTags, ...newTags };
 }
