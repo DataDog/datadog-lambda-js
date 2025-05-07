567b5082823456886d87979be54f321201e58f5c
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-05-07 12:48:18 -0400

    create `report` method to build dogstatsd payload

--- src/metrics/dogstatsd.ts
+++ src/metrics/dogstatsd.ts
@@ -4,6 +4,8 @@ import { SocketType } from "node:dgram";
 export class LambdaDogStatsD {
   private static readonly MIN_SEND_BUFFER_SIZE = 32 * 1024;
   private static readonly SOCKET_TYPE: SocketType = "udp4";
+  private static readonly TAG_RE = /[^\w\d_\-:\/\.]/gu;
+  private static readonly TAG_SUB = "_";
 
   private readonly socket: dgram.Socket;
 
@@ -33,6 +35,24 @@ export class LambdaDogStatsD {
    * Timestamp is seconds since epoch.
    */
   public distribution(metric: string, value: number, tags?: string[], timestamp?: number): void {
+    this.report(metric, "d", value, tags, timestamp);
+  }
+
+  private normalizeTags(tags: string[]): string[] {
+    return tags.map((t) => t.replace(LambdaDogStatsD.TAG_RE, LambdaDogStatsD.TAG_SUB));
+  }
+
+  private report(metric: string, metricType: string, value: number | null, tags?: string[], timestamp?: number): void {
+    if (value == null) {
+      return;
+    }
+    const serializedTags = tags && tags.length ? `|#${this.normalizeTags(tags).join(",")}` : "";
+    const timeStampPart = timestamp != null ? `|T${timestamp}` : "";
+    const payload = `${metric}:${value}|${metricType}${serializedTags}${timeStampPart}`;
+    this.send(payload);
+  }
+
+  private send(packet: string) {
     // TODO
   }
 }
