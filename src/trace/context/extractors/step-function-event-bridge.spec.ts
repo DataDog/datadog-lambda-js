80577e3717daa4cb5b93f65646939cdb7789d5c3
Ryan Strat <ryan.strat@datadoghq.com>
2025-07-14 11:25:04 -0400

    Simplify code

--- src/trace/context/extractors/step-function-event-bridge.spec.ts
+++ src/trace/context/extractors/step-function-event-bridge.spec.ts
@@ -55,7 +55,7 @@ describe("StepFunctionEventBridgeTraceExtractor", () => {
         },
       };
 
-      const extractor = new StepFunctionEventBridgeTraceExtractor(tracerWrapper);
+      const extractor = new StepFunctionEventBridgeTraceExtractor();
 
       const traceContext = extractor.extract(payload);
       expect(traceContext).not.toBeNull();
