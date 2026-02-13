62826d173be1fd961a979dad3f71e4f45c879612
Rithika Narayan <rithika.narayan@datadoghq.com>
2026-02-13 16:47:50 -0500

    Removing debug prints

--- .gitlab/scripts/poll_e2e.sh
+++ .gitlab/scripts/poll_e2e.sh
@@ -22,7 +22,6 @@ echo "Fetching E2E job status from: $URL"
 
 while true; do
     RESPONSE=$(curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" "$URL")
-    echo $RESPONSE
     E2E_JOB_STATUS=$(echo "$RESPONSE" | jq -r '.[] | select(.name=="e2e-test") | .downstream_pipeline.status')
     echo -n "E2E job status: $E2E_JOB_STATUS, "
     if [ "$E2E_JOB_STATUS" == "success" ]; then
