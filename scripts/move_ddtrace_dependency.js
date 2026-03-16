a9fffb5ac314d062a5c32091112318f845641ede
Carles Capell <carles.capell@datadoghq.com>
2026-03-16 07:42:55 +0100

    Remove unused native-appsec binaries + native-appsec version pinned in dd-trace-js

--- scripts/move_ddtrace_dependency.js
+++ scripts/move_ddtrace_dependency.js
@@ -1,4 +1,6 @@
 // Moves the dd-trace dependency from devDependencies to dependencies within package.json.
+// Also promotes selected dd-trace optionalDependencies to direct dependencies so they
+// survive `yarn install --production=true --ignore-optional`.
 // This is used when building the Layer
 
 // USAGE: ./move_dd_trace_dependency.js "$(cat package.json)" > package.json
@@ -10,6 +12,8 @@ moveDependency('@datadog/pprof')
 moveDependency('@opentelemetry/api')
 moveDependency('@opentelemetry/api-logs')
 
+addOptionalFromDdTrace('@datadog/native-appsec')
+
 console.log(JSON.stringify(file, null, 2));
 
 function moveDependency (name) {
@@ -17,3 +21,16 @@ function moveDependency (name) {
   delete file.devDependencies[name];
   file.dependencies[name] = ddTraceVersion;
 }
+
+function addOptionalFromDdTrace (name) {
+  try {
+    const ddTracePkg = require('dd-trace/package.json')
+    const version = ddTracePkg.optionalDependencies?.[name]
+    if (version) {
+      file.dependencies[name] = version
+    }
+  } catch {
+    // dd-trace not installed yet; skip
+  }
+}
+
